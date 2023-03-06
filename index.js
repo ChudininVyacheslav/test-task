3685e9d6db269420d0843d3a2d2be90993a4b1d7
Chudinin Vyacheslav <chudinin@inbox.ru>
2023-03-07 01:42:06 +0300

    create logic

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const inputNode = document.querySelector('.add-tusk__input');
 const btnAddNode = document.querySelector('.add-tusk__btn-add');
-const btnChangeNode = document.querySelector('.add-tusk__btn-change');
-const btnDeleteNode = document.querySelector('.task__btn-delete');
-const btnDoneNode = document.querySelector('.task__btn-done');
 const addTaskNode = document.querySelector('.add-task');
-const taskNode = document.querySelector('.task');
 
 btnAddNode.addEventListener("click", () => {
     addTags();
@@ -15,8 +11,8 @@ const addTags = function () {
     textInput += inputNode.value;
 
     if (textInput == '') {
-        textInput = 'Пример заметки'
-    }
+        textInput = 'Пример заметки';
+    };
 
     let divTag = document.createElement("div");
     divTag.className = 'task';
@@ -41,13 +37,69 @@ const addTags = function () {
 
     btnDeleteTag.addEventListener("click", () => {
         addTaskNode.removeChild(divTag, textTag, btnDeleteTag, btnDoneTag);
+        
+        localStorage.removeItem(textInput);
     });
 
     btnDoneTag.addEventListener("click", () => {
-        inputNode.value = textTag.textContent;;
+        inputNode.value = textTag.textContent;
         addTaskNode.removeChild(divTag, textTag, btnDeleteTag, btnDoneTag);
+
+        localStorage.removeItem(textInput);
     });
+
+    localStorage.setItem(textInput, textInput.toString())
 };
+
 addTags();
 
-// console.log(btnDoneNode.textContent)
+const locStor = function (value) {
+
+    let textInput = '';
+    textInput += value;
+
+    let divTag = document.createElement("div");
+    divTag.className = 'task';
+    addTaskNode.appendChild(divTag);
+
+    let textTag = document.createElement("p");
+    textTag.className = 'task__text';
+    textTag.innerHTML = textInput;
+    divTag.appendChild(textTag);
+
+    let btnDeleteTag = document.createElement("button");
+    btnDeleteTag.className = 'task__btn-delete';
+    btnDeleteTag.innerHTML = 'Удалить';
+    divTag.appendChild(btnDeleteTag);
+
+    let btnDoneTag = document.createElement("button");
+    btnDoneTag.className = 'task__btn-done';
+    btnDoneTag.innerHTML = 'Изменить';
+    divTag.appendChild(btnDoneTag);
+
+    if (textInput == 'Пример заметки') {
+        addTaskNode.removeChild(divTag, textTag, btnDeleteTag, btnDoneTag);
+    };
+
+    inputNode.value = "";
+
+    btnDeleteTag.addEventListener("click", () => {
+        addTaskNode.removeChild(divTag, textTag, btnDeleteTag, btnDoneTag);
+        
+        localStorage.removeItem(textInput);
+    });
+
+    btnDoneTag.addEventListener("click", () => {
+        inputNode.value = textTag.textContent;
+        addTaskNode.removeChild(divTag, textTag, btnDeleteTag, btnDoneTag);
+    });
+
+    localStorage.setItem(textInput, textInput.toString())
+};
+
+const items = { ...localStorage };
+
+const arrItems = Object.keys(items);
+arrItems.forEach(key => {
+    locStor(key)
+});
